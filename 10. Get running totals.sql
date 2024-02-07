c1c7c0cf687a566b5732570565e0b58bec1a1d9e
Tavanhm <146018262+Tavanhm@users.noreply.github.com>
2024-02-06 18:50:29 -0800

    Create 10. Get running totals.sql

---
+++ 10. Get running totals.sql
@@ -0,0 +1,5 @@
+/*
+Tracking Sales Quota Progress over Time
+
+The sales team works diligently to sell their product, and they have quotas that they must reach in order to earn all of their commission. Because these goals are so intimately tied to revenue, the manager of the team wants to track each sales member's performance throughout the year. You suggest a % of quota reached metric that could be displayed on a dashboard, but the sales manager expresses her concern that a single metric won't give her visibility into their progress throughout the year. You suggest providing a running_total of sales revenue and a percent_quota metric that will be recalculated every time a sales member makes another sale. She agrees, and you get started!
+*/
